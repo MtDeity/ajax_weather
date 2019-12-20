fdd9db099c5b01b6942cc77bcbdd4fc29cba6b2d
MtDeity <tagosakufrancokamiyama@yahoo.co.jp>
2019-12-20 14:08:07 +0900

    APIから取得したデータをHTMLに反映

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = "https://api.openweathermap.org/data/2.5/weather";
+const url =　new URL("https://api.openweathermap.org/data/2.5/weather");
 const params = {
   appid: "4b5774e9f3d2a07b84f0f2f88e486224",
   q: "London"
@@ -7,7 +7,12 @@ const qs = new URLSearchParams(params);
 
 fetch(`${url}?${qs}`, {
   method: "GET",
-}).then(response => response.json())
-.then(json => {
-  console.log(json.weather[0].description);
-});
+})
+  .then(response => response.json())
+  .then(json => {
+    const weather = json.weather[0].description;
+    console.log(json.weather[0].description);
+
+    const root = document.getElementById("root");
+    root.innerHTML = `<h1>${weather}</h1>`;
+  });
